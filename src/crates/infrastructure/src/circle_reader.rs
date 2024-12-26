39c057d147fa56e174d47284a21dcdb40f6ce679
katayama800 <tattu.0310@gmail.com>
2024-12-26 14:52:04 +0000

    CircleReader: refactor database interaction to use custom Db type

--- src/crates/infrastructure/src/circle_reader.rs
+++ src/crates/infrastructure/src/circle_reader.rs
@@ -3,24 +3,29 @@ use domain::{
     interface::query::circle_reader_interface::CircleReaderInterface,
 };
 
+use crate::local_db_schema::circle_data::CircleData;
+
+use super::db::Db;
 use anyhow::Error;
-use sqlx::MySqlPool;
 
 #[derive(Clone, Debug)]
 pub struct CircleReader {
-    db: MySqlPool,
+    db: Db,
 }
 
 impl CircleReader {
-    pub fn new(db: sqlx::MySqlPool) -> Self {
+    pub fn new(db: Db) -> Self {
         Self { db }
     }
 }
 
 #[async_trait::async_trait]
 impl CircleReaderInterface for CircleReader {
-    async fn get_circle(&self, _id: CircleId) -> Result<Option<Circle>, Error> {
-        unimplemented!("get_circle")
+    async fn get_circle(&self, id: CircleId) -> Result<Option<Circle>, Error> {
+        match self.db.get::<CircleData, _>(&id.to_string())? {
+            Some(data) => Ok(Some(Circle::try_from(data)?)),
+            None => Err(Error::msg("Circle not found")),
+        }
     }
 
     async fn list_circles(&self) -> Result<Vec<Circle>, Error> {
