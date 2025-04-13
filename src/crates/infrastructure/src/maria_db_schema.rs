ea72d33d3414815d21c590bc25c0eb1c8a5e30ba
katayama8000 <tattu.0310@gmail.com>
2025-04-13 16:21:11 +0900

    Refactor imports in circle_repository and add re-exports in maria_db_schema (#65)

--- src/crates/infrastructure/src/maria_db_schema.rs
+++ src/crates/infrastructure/src/maria_db_schema.rs
@@ -1,2 +1,6 @@
 pub(super) mod circle_event_data;
 pub(super) mod circle_protection_data;
+
+// re-export
+pub(super) use circle_event_data::CircleEventData;
+pub(super) use circle_protection_data::CircleProtectionData;
